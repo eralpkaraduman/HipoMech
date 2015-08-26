d9fe83740c6a004f59edd892c1e4d25f3f835e1d
Eralp Karaduman <eralpkaraduman@gmail.com>
2015-08-27 01:03:54 +0300

    init

---
+++ Hipo Mech/Assets/Gear.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gear : MonoBehaviour {
+
+	public float speed = 0;
+	public Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Rotate(Vector3.back * Time.deltaTime * speed);
+		rigidBody.AddTorque (-speed);
+	}
+}
