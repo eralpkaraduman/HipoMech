19289419eba35c3cb907d3dde09c7a303ef7c0db
Eralp Karaduman <eralpkaraduman@gmail.com>
2015-08-27 05:08:35 +0300

    perfected walk

---
+++ Hipo Mech/Assets/CenterOfMass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenterOfMass : MonoBehaviour {
+
+	public Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		this.rigidBody.centerOfMass = this.transform.localPosition;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.rigidBody.centerOfMass = this.transform.localPosition;
+	}
+}
---
+++ Hipo Mech/Assets/FrameRate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrameRate : MonoBehaviour {
+
+	public int frameRate;
+
+	// Use this for initialization
+	void Start () {
+		Application.targetFrameRate = this.frameRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
